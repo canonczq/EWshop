a6443a58fcfc18dfdf5c20c3f9165b36be76bb17
卡农 <2657260640@qq.com>
2021-04-05 10:10:17 +0800

    完成商城所有功能的开发

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      {{banner}}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {ref, onMounted} from 'vue';
+import {getHomeAllData, getBanner} from "network/home";
 
 export default {
   name: 'Home',
+
+  setup(){
+      const banner = ref([]);
+
+      onMounted(()=>{
+        getHomeAllData().then(res=>{
+
+              banner.value = res.slides;
+
+
+        }).catch(err=>{
+
+        })
+
+      })
+
+      return {
+        banner
+      }
+  },
+
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style>
+  #demo {
+    background:url('~assets/images/3.png') no-repeat;
+    width:100px;
+    height:100px;
+  }
+  .demo1 {
+    color:var(--color-high-text)
+  }
+</style>
